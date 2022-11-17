0e416460b382ec2b0bf9e8c39168d86a7c685426
yangqian <qian.yang2@thoughtworks.com>
2022-11-17 19:57:08 +0800

    [yangqian] #N/A fix: remove columns

--- parts/show_parts_report.py
+++ parts/show_parts_report.py
@@ -16,6 +16,5 @@ def show_open_ticket_report(api: sn_connector_api):
             if report_res.status_code != 200:
                 st.error('生成Daily Open Ticket Report失败！')
             else:
-                columns = ['ticket', 'category', 'summary', 'description', 'ticket open date', 'owner', 'status']
                 st.dataframe(pd.DataFrame(data=json.loads(report_res.text)))
                 st.success('生成Daily Open Ticket Report成功！')
